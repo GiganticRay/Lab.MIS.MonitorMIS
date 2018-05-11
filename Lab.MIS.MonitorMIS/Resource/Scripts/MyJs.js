8945b7d46794b9426084a81b201fd271bbc09f32
DESKTOP-GQ47MEQ\acer <1973078177@qq.com>
2018-05-11 14:05:48 +0800

    钱鸿 添加了奉节县的图层，对清空函数进行改进

--- Lab.MIS.MonitorMIS/Resource/Scripts/MyJs.js
+++ Lab.MIS.MonitorMIS/Resource/Scripts/MyJs.js
@@ -3,9 +3,10 @@
 var map;
 var zoom = 12;
 var open = false;
-var handler;
+var handler, handler1;
 var polygonTool;
 var lineTool, markerTool;
+var bool1 = false, bool2 = false, bool3 = false, bool4 = false, bool5 = false;
 $(document).ready(function () {
     map = new T.Map('mapDiv');
     map.centerAndZoom(new T.LngLat(116.40769, 39.89945), zoom);
@@ -26,34 +27,60 @@ $(document).ready(function () {
     lineTool = new T.PolylineTool(map, config);
     polygonTool = new T.PolygonTool(map, config);
     markerTool = new T.MarkTool(map, { follow: true });
-    $("#MeasureLength").click(function() {
+    $("#MeasureLength").click(function () {
         lineTool.open();
-    });
-    $("#MeasureArea").click(function() {
+        bool1 = true;
+    })
+    $("#MeasureArea").click(function () {
         polygonTool.open();
-    });
-    $("#MeasurearkPoint").click(function() {
+        bool2 = true;
+    })
+    $("#MeasurearkPoint").click(function () {
         editMarker();
         markerTool.open();
-    });
-    $("#MarkLine").click(function() {
+        bool3 = true;
+    })
+    $("#MarkLine").click(function () {
         if (handler) handler.close();
         handler = new T.PolylineTool(map);
         handler.open();
-    });
-    $("#MarkArea").click(function() {
-        if (handler) handler.close();
-        handler = new T.PolygonTool(map);
-        handler.open();
-    });
-    $("#clearOverLays").click(function() {
-        map.clearOverLays();
-    });
+        bool4 = true;
+    })
+    $("#MarkArea").click(function () {
+        if (handler1) handler1.close();
+        handler1 = new T.PolygonTool(map);
+        handler1.open();
+        bool5 = true;
+    })
+    $("#clearOverLays").click(function () {
+        if (bool1) {
+            lineTool.clear();
+        }
+        if (bool2) {
+            polygonTool.clear();
+        }
+        if (bool3) {
+            markerTool.clear();
+        }
+        if (bool4) {
+            handler.clear();
+        }
+        if (bool5) {
+            handler1.clear();
+        }
+    })
 
     //点击登录按钮 
     $("#mine").click(function () {
         openLoginModal();
     });
+    var config2 = {
+        pageCapacity: 10,	//每页显示的数量
+        onSearchComplete: localSearchResult	//接收数据的回调函数
+    };
+    //创建搜索对象
+    localsearch = new T.LocalSearch(map, config2);
+    localsearch.search("奉节县");
 });
 //移动控件的位置
 function MoveControl() {
@@ -63,11 +90,11 @@ function MoveControl() {
 //左窗口的移动
 function MoveLeftWindow(){
     if (open == false) {
-        $("#side_bar").animate({ left: '-337px' }, "fast");
+        $("#side_bar").animate({ left: '-337px' }, 100);
         open = true;
     }
     else {
-        $("#side_bar").animate({ left: '0px' }, "fast");
+        $("#side_bar").animate({ left: '0px' }, 100);
         open = false;
     }
 }
@@ -96,5 +123,48 @@ function openLoginModal() {
     $('#loginModal').modal('show');
  
 }
+//一个没啥用的函数
+function localSearchResult(result) {
+    //清空地图及搜索列表
+
+    area(result.getArea());
+}
+//用于解析行政区边界，并画边界线
+function area(obj) {
+    if (obj) {
+        //坐标数组，设置最佳比例尺时会用到
+        var pointsArr = [];
+        var points = obj.points;
+        for (var i = 0; i < points.length; i++) {
+            var regionLngLats = [];
+            var regionArr = points[i].region.split(",");
+            for (var m = 0; m < regionArr.length; m++) {
+                var lnglatArr = regionArr[m].split(" ");
+                var lnglat = new T.LngLat(lnglatArr[0], lnglatArr[1]);
+                regionLngLats.push(lnglat);
+                pointsArr.push(lnglat);
+            }
+            //创建线对象
+            var line1 = new T.Polyline(regionLngLats, {
+                color: "#191970",
+                weight: 3,
+                opacity: 1,
+                lineStyle: "dashed"
+            });
+            //向地图上添加线
+            map.addOverLay(line1);
+            //创建面对象
+            var polygon1 = new T.Polygon(regionLngLats, {
+                color: "#191970", weight: 3, opacity: 0.5, fillColor: "#8B7B8B", fillOpacity: 0.5
+            });
+            //向地图上添加面
+            map.addOverLay(polygon1);
+        }
+
+        //显示最佳比例尺
+        map.setViewport(pointsArr);
+    }
+}
+
 
 
